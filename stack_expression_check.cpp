26dcbfd0f6847569bf7bd40f163b7c41e029b061
Vinod Pankajakshan <vinu.pankajakshan@gmail.com>
2015-05-29 02:15:28 -0400

    Implement logic to check expression

--- stack_expression_check.cpp
+++ stack_expression_check.cpp
@@ -3,12 +3,54 @@
 #include <stack>
 using namespace std;
 
-int main() {
-    string expression = "{[()]}";
+void check_expression(string expression){
+    stack<char> s;
+
+    cout << "size is " << expression.size() << endl;
 
     for (int i = 0; i < expression.size(); i++) {
-        cout << expression[i];
+        switch(expression[i]){
+            case '{':
+                s.push(expression[i]);
+                break;
+
+            case '[':
+                s.push(expression[i]);
+                break;
+
+            case '(':
+                s.push(expression[i]);
+                break;
+
+            case ')':
+                cout << s.top() << " -- " << expression[i] << endl;
+                if(s.top() != '('){
+                    printf("ERROR: Expression out of balance\n");
+                    return;
+                }
+                s.pop();
+
+            case ']':
+                cout << s.top() << " -- " << expression[i] << endl;
+                if(s.top() != '['){
+                    printf("ERROR: Expression out of balance\n");
+                    return;
+                }
+                s.pop();
+                
+            case '}':
+                cout << s.top() << " -- " << expression[i] << endl;
+                if(s.top() != '{'){
+                    printf("ERROR: Expression out of balance\n");
+                    return;
+                }
+                s.pop();
+        }
     }
-    cout << endl;
-    // stack<char> s;
+    cout << "Expression in balance" << endl;
+}
+
+int main() {
+    string expression = "{[()]}";
+    check_expression(expression);
 }
