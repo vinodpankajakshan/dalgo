9a8e710f59f49a171f5d786f7c59d48622c04131
Vinod Pankajakshan <vinu.pankajakshan@gmail.com>
2015-06-01 02:50:17 -0400

    add stack_brackets_balance.cpp

---
+++ stack_brackets_balance.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+bool check_expression(string expression){
+    stack<char> s;
+
+    for (int i = 0; i < expression.size(); i++) {
+        switch(expression[i]){
+            case '{':
+                s.push(expression[i]);
+                break;
+
+            case '[':
+                s.push(expression[i]);
+                break;
+
+            case '(':
+                s.push(expression[i]);
+                break;
+
+            case ')':
+                if(s.top() != '('){
+                    printf("ERROR: Expression out of balance\n");
+                    return false;
+                }
+                s.pop();
+                break;
+
+            case ']':
+                if(s.top() != '['){
+                    printf("ERROR: Expression out of balance\n");
+                    return false;
+                }
+                s.pop();
+                break;
+                
+            case '}':
+                if(s.top() != '{'){
+                    printf("ERROR: Expression out of balance\n");
+                    return false;
+                }
+                s.pop();
+                break;
+        }
+    }
+    return s.empty() ? true : false;
+}
+
+int main() {
+    string expression;
+    cout << "Enter a brackets pattern: " << endl << ">";
+    cin >> expression;
+    cout << "result: " << check_expression(expression) << endl;
+}
