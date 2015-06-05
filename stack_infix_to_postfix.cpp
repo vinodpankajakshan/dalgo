799bf30d280bfdac667e4eb5e450c35336cf3d37
Vinod Pankajakshan <vinu.pankajakshan@gmail.com>
2015-06-05 00:25:18 -0400

    basic implementation of infix to postfix conversion

--- stack_infix_to_postfix.cpp
+++ stack_infix_to_postfix.cpp
@@ -11,8 +11,10 @@ bool IsOperator(char ch){
     return false;
 }
 
-bool HasHigherPrecedence(char top, char ch){
-    switch(top){
+// Returns true is stack's top has high precedence operator
+// false otherwise.
+bool HasHigher(char ch){
+    switch(s.top()){
         case '/':
             if(ch == '*' or ch == '+' or ch == '-') return true;
             return false;
@@ -38,18 +40,38 @@ bool HasHigherPrecedence(char top, char ch){
     }
 }
 
-void InfixToPostfix(string expr){
+void Evaluate(string expr){
     string post_expr;
 
-    for(int i=0; i< expr.length(); i++){
-        if(IsOperator(expr[i]) && HasHigherPrecedence(s.top(), expr[i])){
+    for(int i=0; i < expr.length(); i++){
+        if(IsOperator(expr[i])){
+            if(s.empty()) s.push(expr[i]);
+            else {
+                if(HasHigher(expr[i])){
+                    while(!s.empty()){
+                        post_expr += s.top();
+                        s.pop();
+                    }
+                }
+                s.push(expr[i]);
+            }
         }
+        else if(i == expr.length() - 1){
+            post_expr += expr[i];
+
+            while(!s.empty()){
+                post_expr += s.top();
+                s.pop();
+            }
+        }
+        else post_expr += expr[i];
     }
+    cout << "postfix expression is " << post_expr << endl;
 }
 
-
 int main(){
-    string expr = "A+B*C-D*E";
-    // InfixToPostfix(expr);
-    HasHigherPrecedence('R', 'V');
+    string expr;
+    cout << "Enter a Infix Expression: " << endl << "> ";
+    getline(cin, expr);
+    Evaluate(expr);
 }
