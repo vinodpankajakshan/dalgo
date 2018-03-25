840e602fea4e4c8dc47ffefaff115fc2e29ff283
Vinod Pankajakshan <vpankajakshn@gmail.com>
2018-03-25 01:04:58 -0400

    implement Python list based queue

---
+++ py/queue.py
@@ -0,0 +1,15 @@
+class Queue(object):
+    '''
+    Python list based queue implementation
+    '''
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        return self.storage.pop(0)
